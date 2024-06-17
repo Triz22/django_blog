65fd7e33faeefaf76fb917423cd2943feb43a9ff
Tomi Akin <tomia01@hotmail.com>
2024-06-17 16:24:10 +0000

    add comment model

--- codestar/settings.py
+++ codestar/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e2!+3bq3+-t!v_ql+5t(p7)q2b(1qvj^#ve#f*v=*o(5zm1!g5'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-triz22-djangoblog-42lbi6ugnlk.ws-eu114.gitpod.io',
@@ -77,12 +83,23 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeanyapp.com",
+    "https://*triz22-djangoblog-42lbi6ugnlk.ws-eu114.gitpod.io",
+    "https://8000-triz22-djangoblog-42lbi6ugnlk.ws-eu114.gitpod.io",
+    "https://*.herokuapp.com"
+]
 
 
 # Password validation
