4d6f47ffc80bfc9cf8e0f164c4ddd50286b3d610
Tomi Akin <tomia01@hotmail.com>
2024-06-21 11:14:51 +0000

    tutor

--- blog/models.py
+++ blog/models.py
@@ -32,7 +32,7 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
-    challenge = models.SlugField()
+    challenge = models.SlugField(null=True)
 
     
     class Meta:
