31bdb28c24b2e67f4a16a1384269d2e13af8d6ba
Tomi Akin <tomia01@hotmail.com>
2024-06-21 16:21:30 +0000

    add functionality to website using javascript

--- about/admin.py
+++ about/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 # Register your models here.
 
 from django.contrib import admin
-from .models import About
+from .models import About, CollaborateRequest
 from django_summernote.admin import SummernoteModelAdmin
 
 
@@ -11,3 +11,8 @@ from django_summernote.admin import SummernoteModelAdmin
 class AboutAdmin(SummernoteModelAdmin):
 
     summernote_fields = ('content',)
+
+@admin.register(CollaborateRequest)
+class CollaborateRequestAdmin(admin.ModelAdmin):
+
+    list_display = ('message', 'read',)
